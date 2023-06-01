57d1f0c98599c01d078e16fb35457091b83ce1f8
Guravyogesh <107176305+Guravyogesh@users.noreply.github.com>
2023-06-01 15:15:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ const server = jsonServer.create();
 const router = jsonServer.router('productList.json'); // <== Will be created later
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 8080; // <== You can change the port
-
+const jsonFiles = [
+    'brandList.json',
+    'categoryList.json',
+//     'path/to/file3.json',
+    // Add more file paths as needed
+  ];
 server.use(middlewares);
 server.use(router);
 
