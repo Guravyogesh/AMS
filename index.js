474b94a4241b7713b9e78801c065e9dc4ba82c41
Guravyogesh <107176305+Guravyogesh@users.noreply.github.com>
2023-06-01 15:00:01 +0530

    Update indexfile

--- index.js
+++ index.js
@@ -1,37 +1,11 @@
-// const jsonServer = require('json-server');
-// const server = jsonServer.create();
-// const router = jsonServer.router('productList.json'); // <== Will be created later
-// const middlewares = jsonServer.defaults();
-// const port = process.env.PORT || 8080; // <== You can change the port
-
-// server.use(middlewares);
-// server.use(router);
-
-// server.listen(port);
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router();
+const router = jsonServer.router('productList.json'); // <== Will be created later
 const middlewares = jsonServer.defaults();
-
-const jsonFiles = [
-  'brandList.json',
-  'categoryList.json',
-  'productList.json',
-  // Add more file paths as needed
-];
-
-let db = {};
-jsonFiles.forEach(file => {
-  const jsonData = require(file);
-  db = { ...db, ...jsonData };
-});
-
-router.db.object = db;
+const port = process.env.PORT || 8080; // <== You can change the port
 
 server.use(middlewares);
 server.use(router);
 
-const port = 3000; // Change to your desired port number
-server.listen(port, () => {
-  console.log(`JSON Server is running on port ${port}`);
-});
+server.listen(port);
+
