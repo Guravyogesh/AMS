a0f0d53348a0908c4bf79602a0a52965e8e7cdfb
Guravyogesh <107176305+Guravyogesh@users.noreply.github.com>
2023-06-02 11:34:27 +0530

    Update index.js file with all files

--- index.js
+++ index.js
@@ -1,26 +1,11 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router();
+const router = jsonServer.router(['brandList.json', 'productList.json', 'categoryList.json']); // <== Will be created later
 const middlewares = jsonServer.defaults();
-
-const jsonFiles = [
-  'brandList.json.json',
-  'productList.json',
-  'categoryList.json',
-  // Add more file paths as needed
-];
-
-let db = {};
-jsonFiles.forEach(file => {
-  const jsonData = require(file);
-  db = { ...db, ...jsonData };
-});
-
-router.db.object = db;
+const port = process.env.PORT || 8080; // <== You can change the port
 
 server.use(middlewares);
 server.use(router);
 
-const port = 8080; // Change to your desired port number
 server.listen(port);
 
