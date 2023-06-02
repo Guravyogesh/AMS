2b3f3dd7986040eac37f2a7a316bdc47e4484f0d
Guravyogesh <107176305+Guravyogesh@users.noreply.github.com>
2023-06-02 11:45:10 +0530

    Update index.js with router

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const jsonServer = require('json-server');
 const path = require('path');
 const server = jsonServer.create();
-const router = jsonServer.router(['brandList.json', 'productList.json', 'categoryList.json']); // <== Will be created later
+const router = jsonServer.router(data); // <== Will be created later
 const middlewares = jsonServer.defaults();
+const posts = ['brandList.json', 'productList.json', 'categoryList.json'];
+const data = { posts };
 server.use(middlewares);
 server.use(router);
-server.listen(3000, () => {
+
+server.listen(8080, () => {
   console.log('JSON Server is running on port 3000');
 });
 
